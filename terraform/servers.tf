e214b59ae409b90c75d3052b34f47066e9fce366
orzehhowski <filmyminecraftnayt@gmail.com>
2025-11-05 13:38:23 +0100

    terraform: rootCA server added

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -40,3 +40,44 @@ resource "proxmox_lxc" "LAB-S03-DHCP-DNS" {
     gw     = "10.120.69.17"
   }
 }
+
+resource "proxmox_lxc" "LAB-S04-RootCA" {
+  vmid        = 105
+  target_node = var.node
+  hostname = "LAB-S04-RootCA"
+  ostemplate = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  ostype = "ubuntu"
+  password = var.recovery_password
+  ssh_public_keys = var.ssh_public_key
+
+  memory = 1024
+  cores = 2
+  swap = 512
+
+  bwlimit = 0
+  cmode = "tty"
+  restore = false
+  force = false
+  ignore_unpack_errors = false
+  start = true
+  template = false
+  unique = false
+  unprivileged = true
+
+  rootfs {
+    acl = false
+    quota = false 
+    replicate = false
+    ro = false
+    shared = false
+    size = "8G"
+    storage = "TestDir"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr1"
+    ip     = "10.120.69.21/28"
+    gw     = "10.120.69.17"
+  }
+}
