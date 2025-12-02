3df3420d1f93a7dceb454cf1beaa70731c8300fc
orzehhowski <filmyminecraftnayt@gmail.com>
2025-12-02 13:17:20 +0100

    terraform: Data and Apps-HA servers added, provider updated

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -82,6 +82,47 @@ resource "proxmox_lxc" "LAB-S04-RootCA" {
   }
 }
 
+resource "proxmox_lxc" "LAB-S05-Data" {
+  vmid        = 107
+  target_node = var.node
+  hostname = "LAB-S05-Data"
+  ostemplate = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  ostype = "ubuntu"
+  password = var.recovery_password
+  ssh_public_keys = var.ssh_public_key
+
+  memory = 1024
+  cores = 2
+  swap = 512
+
+  bwlimit = 0
+  cmode = "tty"
+  restore = false
+  force = false
+  ignore_unpack_errors = false
+  start = true
+  template = false
+  unique = false
+  unprivileged = true
+
+  rootfs {
+    acl = false
+    quota = false 
+    replicate = false
+    ro = false
+    shared = false
+    size = "32G"
+    storage = "TestDir"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr1"
+    ip     = "10.120.69.19/28"
+    gw     = "10.120.69.17"
+  }
+}
+
 resource "proxmox_vm_qemu" "LAB-S11-Apps" {
   name        = "LAB-S11-Apps"
   target_node = var.node
@@ -125,6 +166,56 @@ resource "proxmox_vm_qemu" "LAB-S11-Apps" {
   }
 
   ipconfig0 = "ip=10.120.69.29/28,gw=10.120.69.17"
+
+  sshkeys = var.ssh_public_key
+
+  ciuser  = "fedora"
+  cipassword = ""   # SSH keys only
+}
+
+resource "proxmox_vm_qemu" "LAB-S12-Apps-HA" {
+  name        = "LAB-S12-Apps-HA"
+  target_node = var.node
+
+  # must create template with scripts/create-fedora-base-template.sh
+  clone = "LAB-T01-Fedora"
+
+  os_type = "cloud-init"
+  full_clone = true
+  agent      = 1
+
+  cpu {
+    cores   = 4
+    sockets = 1
+    # fastest virtualization
+    type    = "host"
+  }
+  memory  = 8192
+  # docker host needs stable memory
+  balloon = 0
+
+  scsihw  = "virtio-scsi-pci"
+  disk {
+    size    = "30G"
+    type    = "disk"
+    storage = "TestDir"
+    format  = "raw"
+    slot    = "scsi0"
+  }
+
+  disk {
+    type    = "cloudinit"
+    slot    = "ide0"
+    storage = "TestDir"
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+
+  ipconfig0 = "ip=10.120.69.30/28,gw=10.120.69.17"
 
   sshkeys = var.ssh_public_key
 
